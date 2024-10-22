ef135de340c68469d14eeca262ee6c6b7c61c9de
Ishman Jeet Singh <ishmanalagh@gmail.com>
2024-10-22 21:49:55 +0530

    added tailwind

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,  // Change the port to 3000
+    open: true   // Automatically open the browser on server start
+  }
 })
